7fd172088288df37f5f5dc214abbcd79667dbb23
Ivan Adamov <psnasti@gmail.com>
2023-09-21 18:42:15 +0200

    added workflows

--- tests/test_mentions.py
+++ tests/test_mentions.py
@@ -11,10 +11,10 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.get_tweets(author="google", count=100)
-def test_google_mentions(home_page: HomePage, tweets: list[Tweet]):
+def test_google_mentions(home_page: HomePage, cacheable_tweets: list[Tweet]):
     total_mentions_count = 0
     each_mention_count: DefaultDict[str, int] = defaultdict(int)
-    for tweet in tweets:
+    for tweet in cacheable_tweets:
         for mention in tweet.mentions:
             total_mentions_count += 1
             each_mention_count[mention] += 1
