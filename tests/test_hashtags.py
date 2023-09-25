a19e1ae82b99685c745c25fe3787597ac19cec59
Ivan Adamov <psnasti@gmail.com>
2023-09-25 09:16:55 +0200

    added fetching the confirmation code

--- tests/test_hashtags.py
+++ tests/test_hashtags.py
@@ -11,13 +11,13 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.get_tweets(author="google", count=100)
-def test_google_hash_tags(home_page: HomePage, cacheable_tweets: list[Tweet]):
+def test_google_hashtags(home_page: HomePage, cacheable_tweets: list[Tweet]):
     total_hash_tags_count = 0
     each_hash_tag_count: DefaultDict[str, int] = defaultdict(int)
     for tweet in cacheable_tweets:
-        for hash_tag in tweet.hash_tags:
+        for hash_tag in tweet.hashtags:
             total_hash_tags_count += 1
             each_hash_tag_count[hash_tag] += 1
 
     logger.info(f"Total hash tags in 100 tweets: {total_hash_tags_count}")
-    logger.info(f"Each hash tag count: {dict(each_hash_tag_count)}")
+    logger.info(f"Each hashtag count: {dict(each_hash_tag_count)}")
